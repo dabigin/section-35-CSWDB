e2efbd50eaf2004ff55986b6a2f774ba0bf4e059
Brandon Dalton <dabigin@gmail.com>
2023-05-11 09:15:13 -0500

    Section 35 Lecture 362 Parsing The Request Body

--- RESTdemo/index.js
+++ RESTdemo/index.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const app = express();
 
+// for parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }))
+// for parsing JSON
+app.use(express.json())
+
 app.get("/tacos", (req, res) => {
     res.send("GET /tacos response")
 })
 
 app.post("/tacos", (req, res) => {
-    res.send("POST /tacos response")
+    const { meat, qty } = req.body;
+    res.send(`OK, here are your ${qty} ${meat} tacos`)
 })
 
 app.listen(3000, () => {
