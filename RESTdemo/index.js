0591ccff87f33fd203dc0d28f7412e26798b729d
Brandon Dalton <dabigin@gmail.com>
2023-05-11 19:49:38 -0500

    Section 35 Lecture 365 RESTful Comments Index

--- RESTdemo/index.js
+++ RESTdemo/index.js
@@ -1,10 +1,36 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 // for parsing application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }))
 // for parsing JSON
 app.use(express.json())
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "ejs")
+
+const comments = [
+    {
+        username: "Todd",
+        comment: "lol that is so funny!"
+    },
+    {
+        username: "Skyler",
+        comment: "I like to go birdwatching with my dog"
+    },
+    {
+        username: "Sk8erBoi",
+        comment: "Plz delete your account, Todd"
+    },
+    {
+        username: "onlysayswoof",
+        comment: "woof woof woof"
+    }
+]
+
+app.get("/comments", (req, res) => {
+    res.render("comments/index", { comments })
+})
 
 app.get("/tacos", (req, res) => {
     res.send("GET /tacos response")
