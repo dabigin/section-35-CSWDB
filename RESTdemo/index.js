595fec1b82c6e25ec1f344471f3e467a7298cb70
Brandon Dalton <dabigin@gmail.com>
2023-05-20 11:34:45 -0500

    Section 35 Lecture 372 RESTful Comments Delete

--- RESTdemo/index.js
+++ RESTdemo/index.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require("path");
 const methodOverride = require("method-override")
 const { v4: uuid } = require('uuid');
-uuid();
+
 
 // for parsing application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }))
@@ -14,7 +14,7 @@ app.use(methodOverride("_method"))
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
 
-const comments = [
+let comments = [
     {
         id: uuid(),
         username: "Todd",
@@ -38,7 +38,7 @@ const comments = [
 ]
 
 app.get("/comments", (req, res) => {
-    res.render("comments/index", { comments })
+    res.render("comments/index", { comments });
 })
 
 app.get("/comments/new", (req, res) => {
@@ -47,7 +47,7 @@ app.get("/comments/new", (req, res) => {
 
 app.post("/comments", (req, res) => {
     const { username, comment } = req.body;
-    comments.push({ username, comment, id: uuid() })
+    comments.push({ username, comment, id: uuid() });
     res.redirect("/comments");
 })
 
@@ -60,7 +60,7 @@ app.get("/comments/:id", (req, res) => {
 app.get("/comments/:id/edit", (req, res) => {
     const { id } = req.params;
     const comment = comments.find(c => c.id === id);
-    res.render("comments/edit", { comment })
+    res.render("comments/edit", { comment });
 })
 
 app.patch("/comments/:id", (req, res) => {
@@ -68,20 +68,26 @@ app.patch("/comments/:id", (req, res) => {
     const newCommentText = req.body.comment;
     const foundComment = comments.find(c => c.id === id);
     foundComment.comment = newCommentText;
-    res.redirect("/comments")
+    res.redirect("/comments");
+})
+
+app.delete("/comments/:id", (req, res) => {
+    const { id } = req.params;
+    comments = comments.filter(c => c.id !== id);
+    res.redirect("/comments");
 })
 
 app.get("/tacos", (req, res) => {
-    res.send("GET /tacos response")
+    res.send("GET /tacos response");
 })
 
 app.post("/tacos", (req, res) => {
     const { meat, qty } = req.body;
-    res.send(`OK, here are your ${qty} ${meat} tacos`)
+    res.send(`OK, here are your ${qty} ${meat} tacos`);
 })
 
 app.listen(3000, () => {
-    console.log("ON PORT 3000!")
+    console.log("ON PORT 3000!");
 })
 
 // GET /comments - list all comments
